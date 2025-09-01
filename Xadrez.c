91103ed2a09152ff1ffa784ab806d88f6c556fb5
Enzo Andrade Ferreira <112990187+ENZOANDRADEF@users.noreply.github.com>
2025-09-01 21:30:51 +0000

    Recursividade

--- Xadrez.c
+++ Xadrez.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+void moverTorre(int casas){
+    if(casas > 0){
+        printf("Direita\n");
+        moverTorre(casas - 1);
+    }
+}
+void moverBispo(int casas){
+    if(casas > 0){
+        printf("Cima, Direita\n");
+        moverBispo(casas - 1);
+    }
+}
+void moverRainha(int casas){
+    if(casas > 0){
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+
 int main(){
 
     char peca;
@@ -17,28 +37,17 @@ int main(){
     {
     case 'T':
     case 't':
-        for (int i = 0; i < 5; i++)
-        {
-            printf("Direita\n");
-        }
+            moverTorre(5);
         break;
 
     case 'B':
     case 'b':
-        while (i < 5)
-        {
-            printf("Cima, Direita\n");
-            i++;
-        }
+            moverBispo(5);
         break;
 
     case 'R':
     case 'r':
-        do
-        {
-            printf("Esquerda\n");
-            i++;
-        } while (i<8);
+            moverRainha(8);
         break;
     case 'C':
     case 'c':
